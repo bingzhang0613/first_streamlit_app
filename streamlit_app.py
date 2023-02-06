50b04aeed90f79a6f07965eefcf1685fbc704788
bingzhang0613 <123669774+bingzhang0613@users.noreply.github.com>
2023-02-06 14:03:41 -0500

    Update streamlit_app.py
    Add Try-Except

--- streamlit_app.py
+++ streamlit_app.py
@@ -31,18 +31,32 @@ streamlit.dataframe(my_fruit_list)
 
 # New Section to display fruitcvice api response
 streamlit.header("Fruityvice Fruit Advice!")
-fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
-streamlit.write('The user entered ', fruit_choice)
+try:
+  fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
+  #streamlit.write('The user entered ', fruit_choice)
+  if not fruit_choice:
+    streamlit.error("Please select a fruit to get information.")
+  else:
+    fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
+    # take the json version of the response and normalize it
+    fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+    # output it the screen as a table
+    streamlit.dataframe(fruityvice_normalized)
 
-# Import requests package and get response
-#import requests
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice) #separate the base URL from the fruit name
-# streamlit.text(fruityvice_response.json()) # just writes the data to the screen --> comment out
+except URLErrror as e:
+  streamlit.error()
+    
+    
 
-# put json format
-fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-# put data to dataframe
-streamlit.dataframe(fruityvice_normalized)
+# # Import requests package and get response
+# #import requests
+# fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice) #separate the base URL from the fruit name
+# # streamlit.text(fruityvice_response.json()) # just writes the data to the screen --> comment out
+
+# # put json format
+# fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+# # put data to dataframe
+# streamlit.dataframe(fruityvice_normalized)
 
 
 # don't run anything past here while we troubleshoot
