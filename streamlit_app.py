fcddf8f667e295c1905a4991c586845f9b8530f4
bingzhang0613 <123669774+bingzhang0613@users.noreply.github.com>
2023-02-06 13:29:47 -0500

    Update streamlit_app.py
    add another interaction of text entry

--- streamlit_app.py
+++ streamlit_app.py
@@ -57,3 +57,7 @@ streamlit.header("The fruit load list contains:")
 #streamlit.dataframe(my_data_row)
 streamlit.dataframe(my_data_rows)
 
+
+# New Section for another text entry
+fruit_choice2 = streamlit.text_input('What fruit would you like to add?','jackfruit')
+streamlit.write('Thanks for adding ', fruit_choice2)
